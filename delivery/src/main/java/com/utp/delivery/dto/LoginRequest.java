eea313eae7eeaeb1e9e5515aff7e50ef82fce428
Patrick-Drz <patrickdelaguilardz@gmail.com>
2025-10-14 23:36:05 -0500

    avance dto 1

---
+++ delivery/src/main/java/com/utp/delivery/dto/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.utp.delivery.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder @AllArgsConstructor @NoArgsConstructor
+public class LoginRequest {
+    private String correo;
+    private String contrasena;
+}
