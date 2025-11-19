5dcc2eb27f84f315de4b96622d4c63c3f0c0c6f1
kar1uz <lion571lex@gmail.com>
2025-11-19 00:15:45 -0500

    correcion productos

--- delivery/src/main/java/com/utp/delivery/config/SecurityConfig.java
+++ delivery/src/main/java/com/utp/delivery/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.utp.delivery.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,6 +28,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/productos/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
